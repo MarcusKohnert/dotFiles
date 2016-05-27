d88656a720459afbf23e78aea1637d8e8cef2a25
Marcus Kohnert <marcus.dev@live.com>
2016-05-27 08:19:50 +0200

    added git lols

--- git/gitConfigSetup.bat
+++ git/gitConfigSetup.bat
@@ -1,3 +1,4 @@
 git config --global alias.lg log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit
 git config --global alias.lol log --oneline --graph --decorate
+git config --global alias.lols !"git lol -10"
 git config --global core.editor "'D:/Program Files/Sublime Text 3/subl.exe' -w"
