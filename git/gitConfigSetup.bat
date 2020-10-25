18aa6aed1e76c6cc85bd3925f34b8843cc04d6d5
Marcus Kohnert <marcus.dev@live.com>
2020-10-25 10:33:00 +0100

    Activates diff3 conflict style

--- git/gitConfigSetup.bat
+++ git/gitConfigSetup.bat
@@ -2,3 +2,4 @@ git config --global alias.lg log --color --graph --pretty=format:'%Cred%h%Creset
 git config --global alias.lol log --oneline --graph --decorate
 git config --global alias.lols !"git lol -10"
 git config --global core.editor "'D:/Program Files/Sublime Text 3/subl.exe' -w"
+git config --global merge.conflictstyle
